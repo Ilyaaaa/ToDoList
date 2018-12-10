64c86afc97a3139911477ff1ca2822c41b2703e1
Ilyaaaa <ilyaaa.1997.13@gmail.com>
2018-12-10 12:16:12 +0200

    task form fixes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
-    validates :title, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "contains special characters" }
-    validates :description, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "contains special characters" }, allow_blank: true
+    validates :title, presence: true
+    validates :description, presence: true, allow_blank: true
     validates :priority, presence: true, numericality: { only_integer: true }
     validates :due_date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     before_create :confirmation_token
     
-    validates :firstname, presence: true, length: {minimum:1, maximum: 30}, format: { with: /\A[a-zA-Z]+\z/, message: "First name contains special characters" }
-    validates :lastname, presence: true, length: {minimum:1, maximum: 30}, format: { with: /\A[a-zA-Z]+\z/, message: "Last name contains special characters" }
+    validates :firstname, presence: true, length: {minimum:1, maximum: 30}
+    validates :lastname, presence: true, length: {minimum:1, maximum: 30}
     validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
     validates :pass, presence: true, length: {minimum:8, maximum:64}, confirmation: true
     validates :pass_confirmation, presence: true
